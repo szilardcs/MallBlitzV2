2a75c1af1c948bf48e0fbaedd7c4e2ffc707c5df
Szilardcs <cs.sziki@gmail.com>
2026-03-14 13:38:50 +0100

    Tweaked base component

--- E2E/pages/components/BaseComponent.ts
+++ E2E/pages/components/BaseComponent.ts
@@ -3,7 +3,7 @@ import { Page } from "@playwright/test";
 export abstract class BaseComponent {
 	constructor(protected readonly page: Page) {}
 
-	protected async goToHomePage() {
+	protected async goToHomePage(): Promise<void> {
 		await this.page.goto("");
 	}
 }
